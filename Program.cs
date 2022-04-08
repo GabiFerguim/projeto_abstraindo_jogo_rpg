3db3bb20e2969b00b57d08fdb04e41de91fdf953
Gabriela Penha <gabriela.f.guimaraes@gmail.com>
2022-04-07 21:28:38 -0300

    add projeto que abstrai jogo de rpg

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using dotnet_poo.src.Entities;
+
+namespace dotnet_poo // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+      {
+        Knight arus = new Knight("Arus", 23, "Knight"); 
+        Wizard wizard = new Wizard("Jennica", 23, "White Wizard");
+    
+
+        Console.WriteLine(wizard.Attack(1));
+        Console.WriteLine(wizard.Attack(7));
+
+      }
+        }
+    }
